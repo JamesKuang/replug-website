fd3d5b870b5018e97fb9a279d5fa8c2bda9aa575
James Kuang <kuang812@gmail.com>
2020-06-20 00:25:07 -0400

    Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <div>
+        <h1>Replug for iOS!</h1>
+        <p>Tesla Route Planner</p>
+        <a href="https://apps.apple.com/us/app/replug/id1279136790">Download from App Store!</a>
+      </div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div >
+  )
 }
