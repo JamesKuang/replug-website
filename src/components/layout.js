bb54cd795c4e73bf59936fc58068e636dc01105f
James Kuang <kuang812@gmail.com>
2020-06-20 21:54:33 -0400

    Add Privacy Policy and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li style={{ display: `inline-block`, marginLeft: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
@@ -12,15 +12,21 @@ export default function Layout({ children }) {
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>Replug</h3>
+          <h2 style={{ display: `inline` }}>Replug</h2>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
-          <ListLink to="/">Home</ListLink>
+          <ListLink to="https://apps.apple.com/us/app/replug/id1279136790">Download</ListLink>
           <ListLink to="/about">About</ListLink>
           <ListLink to="/help">Help</ListLink>
         </ul>
       </header>
       {children}
+      <footer style={{ marginTop: `1.5rem` }}>
+        <ul style={{ listStyle: `none`, float: `none` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/privacy-policy">Privacy Policy</ListLink>
+        </ul>
+      </footer>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <div>
-        <Link to="/contact/">Contact</Link>
-        <h1>Replug for iOS!</h1>
+        <h1>Replug</h1>
         <p>Tesla Route Planner</p>
         <a href="https://apps.apple.com/us/app/replug/id1279136790">Download from App Store!</a>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
